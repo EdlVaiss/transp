1faabd5694df2411c42e8c78f0514d101e77e506
Miliuta Siarhei <vaissedl@gmail.com>
2019-04-22 17:42:22 +0300

    aggregate function added to selection list

--- dao/src/main/java/com/senlainc/miliuta/dao/report/utils/SelectionListFactory.java
+++ dao/src/main/java/com/senlainc/miliuta/dao/report/utils/SelectionListFactory.java
@@ -3,21 +3,25 @@ package com.senlainc.miliuta.dao.report.utils;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.Path;
 import javax.persistence.criteria.Selection;
 
 public class SelectionListFactory<K> {
-	private Path<K> path;
+	private Path<K> root;
+	private CriteriaBuilder criteriaBuilder;
+	private final String AGGREGATE_FIELD = "cost";
 
-	public SelectionListFactory(Path<K> root) {
-		this.path = root;
+	public SelectionListFactory(final Path<K> root, final CriteriaBuilder criteriaBuilder) {
+		this.root = root;
+		this.criteriaBuilder = criteriaBuilder;
 	}
 
 	public List<Selection<?>> getSelectionList(List<String> stringList) {
-		ExpressionBuilder<K> eb = new ExpressionBuilder<>(path);
+		ExpressionBuilder<K> eb = new ExpressionBuilder<>(root);
 
 		List<Selection<?>> slectionList = stringList.stream().map(s -> eb.build(s)).collect(Collectors.toList());
-		
+		slectionList.add(criteriaBuilder.sum(root.get(AGGREGATE_FIELD)));
 		return slectionList;
 	}
 
