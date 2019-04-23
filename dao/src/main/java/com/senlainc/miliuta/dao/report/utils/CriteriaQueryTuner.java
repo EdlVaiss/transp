61f5a4a9abbeb6e80bbdff7e6d8c9c4e8058886f
Miliuta Siarhei <vaissedl@gmail.com>
2019-04-23 11:32:09 +0300

    tuneGroupBy() method realised

--- dao/src/main/java/com/senlainc/miliuta/dao/report/utils/CriteriaQueryTuner.java
+++ dao/src/main/java/com/senlainc/miliuta/dao/report/utils/CriteriaQueryTuner.java
@@ -5,17 +5,18 @@ import java.util.List;
 
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Expression;
 import javax.persistence.criteria.Path;
+import javax.persistence.criteria.Selection;
 
-
-public class CriteriaQueryTuner<T> implements ICriteriaQueryTuner<T>{
+public class CriteriaQueryTuner<T> implements ICriteriaQueryTuner<T> {
 	private CriteriaBuilder criteriaBuilder;
 	private CriteriaQuery<Object[]> criteriaQuery;
 	private Path<T> root;
 
-	//TODO for test purposes, remove after use 
-	private final List<String> str = Arrays.asList("car.brand","mileage");
-	
+	// TODO for test purposes, remove after use
+	private final List<String> str = Arrays.asList("car.brand", "mileage");
+
 	public CriteriaQueryTuner(CriteriaBuilder criteriaBuilder, Class<T> clazz) {
 		this.criteriaBuilder = criteriaBuilder;
 		criteriaQuery = criteriaBuilder.createQuery(Object[].class);
@@ -30,7 +31,8 @@ public class CriteriaQueryTuner<T> implements ICriteriaQueryTuner<T>{
 	@Override
 	public CriteriaQueryTuner<T> tuneSelect() {
 		SelectionListFactory<T> slf = new SelectionListFactory<>(root, criteriaBuilder);
-		criteriaQuery.multiselect(slf.getSelectionList(str));
+		List<Selection<?>> slectionList = slf.getSelectionList(str);
+		criteriaQuery.multiselect(slectionList);
 		return this;
 	}
 
@@ -42,10 +44,13 @@ public class CriteriaQueryTuner<T> implements ICriteriaQueryTuner<T>{
 
 	@Override
 	public CriteriaQueryTuner<T> tuneGroupBy() {
-		// TODO hardcoded method, needs to be changed
-		criteriaQuery.groupBy(root.get("car").get("brand"));
+		Selection<?> selection = criteriaQuery.getSelection();
+		if (selection.isCompoundSelection()) {
+			criteriaQuery.groupBy((Expression<?>) selection.getCompoundSelectionItems().get(0));
+		} else {
+			criteriaQuery.groupBy((Expression<?>) selection);
+		}
 		return this;
 	}
-	
-	
+
 }
