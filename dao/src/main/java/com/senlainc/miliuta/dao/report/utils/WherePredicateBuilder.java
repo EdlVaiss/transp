227fb2f5cc639633f533074e82d4061aa377631c
Miliuta Siarhei <vaissedl@gmail.com>
2019-04-25 16:26:23 +0300

    build method almost complete (dates support needed)

--- dao/src/main/java/com/senlainc/miliuta/dao/report/utils/WherePredicateBuilder.java
+++ dao/src/main/java/com/senlainc/miliuta/dao/report/utils/WherePredicateBuilder.java
@@ -1,22 +1,41 @@
 package com.senlainc.miliuta.dao.report.utils;
 
+import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.Expression;
 import javax.persistence.criteria.Path;
 import javax.persistence.criteria.Predicate;
 
 import com.senlainc.miliuta.model.report.WherePref;
+import com.senlainc.miliuta.model.report.valueholders.ValueHolderFactory;
 
 public class WherePredicateBuilder<T> {
 	private Path<T> root;
+	private CriteriaBuilder criteriaBuilder;
 
-	public WherePredicateBuilder(Path<T> root) {
+	public WherePredicateBuilder(Path<T> root, CriteriaBuilder criteriaBuilder) {
 		this.root = root;
+		this.criteriaBuilder = criteriaBuilder;
+	}
+
+	private Expression<T> getPath(String stringPath) {
+		ExpressionBuilder<T> eb = new ExpressionBuilder<>(root);
+		Expression<T> path = eb.build(stringPath);
+		return path;
 	}
 	
 	public Predicate build(WherePref pref) {
-		ExpressionBuilder<T> eb = new ExpressionBuilder<>(root);
-		Expression<T> path = eb.build(pref.getPath());
+		Expression<T> path = getPath(pref.getPath());
+		ValueHolderFactory vhf = new ValueHolderFactory(pref.getParam());
 		
-		return null;
+		switch (pref.getOperator()) {
+		case eq:
+			return criteriaBuilder.equal(path, vhf.getValueHolder().getValue());
+		case lt:
+			return criteriaBuilder.lt((Expression<Number>)path, (Number)(vhf.getValueHolder().getValue()));
+		case gt:
+			return criteriaBuilder.gt((Expression<Number>)path, (Number)(vhf.getValueHolder().getValue()));
+		default:
+			throw new UnsupportedOperationException();
+		}
 	}
 }
