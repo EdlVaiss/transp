1cbc61d96837185e80537902a88f329693f1dbbe
Miliuta Siarhei <vaissedl@gmail.com>
2019-04-26 15:04:43 +0300

    use metamodel class

--- dao/src/main/java/com/senlainc/miliuta/dao/report/utils/SelectionListFactory.java
+++ dao/src/main/java/com/senlainc/miliuta/dao/report/utils/SelectionListFactory.java
@@ -1,23 +1,29 @@
 package com.senlainc.miliuta.dao.report.utils;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.Path;
 import javax.persistence.criteria.Selection;
 
-import com.senlainc.miliuta.model.CarExpense_;
+import com.senlainc.miliuta.model.AbstractExpense_;
 
-public class SelectionListFactory<T> extends AbstractSelectionListFactory<T> {
+public class SelectionListFactory<T> {
+	private Path<T> root;
+	private CriteriaBuilder criteriaBuilder;
+	private static final String AGGREGATE_BY_ATTR = AbstractExpense_.COST;
 	
 	public SelectionListFactory(Path<T> root, CriteriaBuilder criteriaBuilder) {
-		super(root, criteriaBuilder);
+		this.root = root;
+		this.criteriaBuilder = criteriaBuilder;
 	}
 
-	@Override
 	public List<Selection<?>> getSelectionList(List<String> stringList) {
-		List<Selection<?>> slectionList = super.getSelectionList(stringList);
-		slectionList.add(criteriaBuilder.sum(root.get(CarExpense_.COST)));
+		ExpressionBuilder<T> eb = new ExpressionBuilder<>(root);
+
+		List<Selection<?>> slectionList = stringList.stream().map(s -> eb.build(s)).collect(Collectors.toList());
+		slectionList.add(criteriaBuilder.sum(root.get(AGGREGATE_BY_ATTR)));
 		return slectionList;
 
 	}
