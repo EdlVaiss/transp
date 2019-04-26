b7b42809f66a5e03547c75ea8baf0dee9bfe7c68
Miliuta Siarhei <vaissedl@gmail.com>
2019-04-26 11:19:33 +0300

    rename WherePredicateArrayFactory to WherePredicateListFactory. Change get method to return List<Predicate>

--- dao/src/main/java/com/senlainc/miliuta/dao/report/utils/WherePredicateListFactory.java
+++ dao/src/main/java/com/senlainc/miliuta/dao/report/utils/WherePredicateListFactory.java
@@ -1,6 +1,7 @@
 package com.senlainc.miliuta.dao.report.utils;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.Path;
@@ -8,20 +9,20 @@ import javax.persistence.criteria.Predicate;
 
 import com.senlainc.miliuta.model.report.WherePref;
 
-public class WherePredicateArrayFactory<T> {
+public class WherePredicateListFactory<T> {
 	private Path<T> root;
 	private CriteriaBuilder criteriaBuilder;
 
-	public WherePredicateArrayFactory(Path<T> root, CriteriaBuilder criteriaBuilder) {
+	public WherePredicateListFactory(Path<T> root, CriteriaBuilder criteriaBuilder) {
 		this.root = root;
 		this.criteriaBuilder = criteriaBuilder;
 	}
 
-	public Predicate[] getWherePredicateArray(List<WherePref> wherePrefs) {
+	public List<Predicate> getWherePredicateList(List<WherePref> wherePrefs) {
 		WherePredicateBuilder<T> predicateBuilder = new WherePredicateBuilder<>(root, criteriaBuilder);
 
-		Predicate[] wherePredicates = wherePrefs.stream().map(pred -> predicateBuilder.build(pred))
-				.toArray(size -> new Predicate[size]);
+		List<Predicate> wherePredicates = wherePrefs.stream().map(pred -> predicateBuilder.build(pred))
+				.collect(Collectors.toList());
 		return wherePredicates;
 	}
 }
