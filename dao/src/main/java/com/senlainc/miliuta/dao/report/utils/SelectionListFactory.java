3870bc100c0a70b7d4dccb5e9429305022ebdf41
Miliuta Siarhei <vaissedl@gmail.com>
2019-04-26 10:29:26 +0300

    changes to support AbstractSelectionListFactory

--- dao/src/main/java/com/senlainc/miliuta/dao/report/utils/SelectionListFactory.java
+++ dao/src/main/java/com/senlainc/miliuta/dao/report/utils/SelectionListFactory.java
@@ -1,28 +1,24 @@
 package com.senlainc.miliuta.dao.report.utils;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.Path;
 import javax.persistence.criteria.Selection;
 
-public class SelectionListFactory<T> {
-	private Path<T> root;
-	private CriteriaBuilder criteriaBuilder;
-	private final String AGGREGATE_FIELD = "cost";
+import com.senlainc.miliuta.model.CarExpense_;
 
+public class SelectionListFactory<T> extends AbstractSelectionListFactory<T> {
+	
 	public SelectionListFactory(Path<T> root, CriteriaBuilder criteriaBuilder) {
-		this.root = root;
-		this.criteriaBuilder = criteriaBuilder;
+		super(root, criteriaBuilder);
 	}
 
+	@Override
 	public List<Selection<?>> getSelectionList(List<String> stringList) {
-		ExpressionBuilder<T> eb = new ExpressionBuilder<>(root);
-
-		List<Selection<?>> slectionList = stringList.stream().map(s -> eb.build(s)).collect(Collectors.toList());
-		slectionList.add(criteriaBuilder.sum(root.get(AGGREGATE_FIELD)));
+		List<Selection<?>> slectionList = super.getSelectionList(stringList);
+		slectionList.add(criteriaBuilder.sum(root.get(CarExpense_.COST)));
 		return slectionList;
-	}
 
+	}
 }
